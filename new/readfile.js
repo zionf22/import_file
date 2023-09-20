c3bbea53be18fba49d6fce3524d4d5414d319255
Zion J <zfontana22@gmail.com>
2023-09-20 16:04:21 -0300

    atualizar

--- new/readfile.js
+++ new/readfile.js
@@ -1,2 +1,28 @@
-import readfile from "fs"
+import {readFile} from "fs";
 
+import { Request, Response, Router} from "express";
+
+import multer from "multer";
+import { request } from "http";
+
+const multerconfig = multer()
+
+const router = Router();
+
+router.post(
+    "/products",
+    multerconfig.single("file")
+    (request: Request, response: Response) == {
+        const {file} = request;
+        const {buffer} = file;
+
+        const readableFile = new Readable();
+        readableFile.push(buffer);
+        readableFile.push(null);
+
+        
+        return response.send();
+    }
+);
+
+export {router};
